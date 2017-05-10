ff7eb34b476444e110f44ebe5d651290b6dfc50d
Phillip Knoll <phillip@phkn.net>
2017-05-09 21:40:36 -0500

    Codelab Functions, Page 7-8 https://codelabs.developers.google.com/codelabs/firebase-cloud-functions/

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -14,9 +14,28 @@
  * limitations under the License.
  */
 
+// Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  console.log('A new user signed in for the first time.');
+  const fullName = user.displayName || 'Anonymous';
+
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  return admin.database().ref('messages').push({
+    name: 'Firebase Bot',
+    photoUrl: '/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!`
+  });
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
